816be9253036ee5f3d03b9bdb25156f875e05eca
Aditya Raghuvanshi <araghuvanshi3841@gmail.com>
2023-01-22 09:35:32 +0530

    Initial Commit

---
+++ controllers/postsController.go
@@ -0,0 +1,93 @@
+package controllers
+
+import (
+	"crudapi/initializers"
+	"crudapi/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func PostsCreate(c *gin.Context) {
+	//Get data off req body
+	var body struct {
+		Body  string
+		Title string
+	}
+	c.Bind(&body)
+
+	//Create a post
+	post := models.Post{Title: body.Title, Body: body.Body}
+	result := initializers.DB.Create(&post)
+
+	if result.Error != nil {
+		c.Status(400)
+		return
+	}
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsIndex(c *gin.Context) {
+	//Get the post
+	var posts []models.Post
+	initializers.DB.Find(&posts)
+
+	//Respond with them
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+}
+
+func PostsShow(c *gin.Context) {
+	//Get id off url
+	id := c.Param("id")
+
+	//Get the posts
+	var post models.Post
+	initializers.DB.First(&post, id)
+
+	//Respond with them
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsUpdate(c *gin.Context) {
+	//Get the id off the url
+	id := c.Param("id")
+
+	//Get the data off the req body
+	var body struct {
+		Body  string
+		Title string
+	}
+
+	c.Bind(&body)
+
+	//Find the post were updating
+	var post models.Post
+	initializers.DB.First(&post, id)
+
+	//update it
+	initializers.DB.Model(&post).Updates(models.Post{
+		Title: body.Title,
+		Body:  body.Body,
+	})
+
+	//Response with it
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsDelete(c *gin.Context) {
+	//Get the id off the url
+	id := c.Param("id")
+
+	//Delete the posts
+	initializers.DB.Delete(&models.Post{}, id)
+
+	//Respond
+	c.Status(200)
+}
